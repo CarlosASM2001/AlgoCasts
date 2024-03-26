0497cbc5f735e9b31a6d5a300590e0c1175ad602
Carlos Serrano <calsemo2001@gmail.com>
2024-03-25 21:04:20 -0400

    Ejercicio Reverse String

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,96 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+/*
+function reverse(str) {
+    return str.split('').reduce((rev, char) =>  char + rev, '');
+}*/
+
+/*
+function reverse(str) {
+
+    let reversed= '';
+
+
+    for(let character of str){
+        reversed=character+reversed;
+    }
+
+    return reversed;
+
+}*/
+
+/*
+function reverse(str) {
+
+    return str.split('').reverse().join('');
+
+}
+*/
+
+/*
+
+function reverse(str) {
+
+    const stack=new Stack(); 
+    let reversechar='';
+
+
+    for (char of str){
+
+        stack.push(char);
+        
+    }
+
+    stack.print();
+
+    while(stack.size()>0){
+        
+        reversechar=stack.pop() + reversechar;
+        
+    }
+
+    return reversechar;
+
+
+
+}
+
+class Stack{
+    constructor(){
+        this.stack={};
+        this.count=0;
+    }
+
+    push(element){
+        this.stack[this.count]=element
+        this.count++;
+        return this.stack
+    }
+    pop(){
+        this.count--;
+        const element=this.stack[this.count];
+        delete this.stack[this.count];
+        return element;
+    }
+
+    peek(){
+        return this.stack[this.count -1];
+
+    }
+
+    size(){
+        return this.count;
+    }
+    print(){
+        console.log(this.stack);
+    }
+       
+    
+
+}
+*/
+
+console.log(reverse('hello'));
 
 module.exports = reverse;
